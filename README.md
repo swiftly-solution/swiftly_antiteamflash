2b74e5cb713a0c0147056a86d5987bf97fb8eaa7
blu133721 <tiganinpungasrl@gmail.com>
2024-02-15 21:38:48 +0200

    (init): Commit files

---
+++ README.md
@@ -0,0 +1,28 @@
+<br/>
+<p align="center">
+  <a href="https://github.com/swiftly-solution/swiftly_antiflash">
+    <img src="https://media.discordapp.net/attachments/979452783466000466/1168236894652469248/Swiftly_Logo.png?ex=6575f264&is=65637d64&hm=dd2834983bebeab98d7febd44bb3bd20e9aded13ecefac63cc990b222a9d9e9e&=&format=webp&quality=lossless&width=468&height=468" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">[Swiftly] Rank System</h3>
+
+  <p align="center">
+    A simple plugin for Swiftly that implements an Rank System.
+    <br/>
+    <br/>
+    <a href="https://github.com/swiftly-solution/swiftly_antiflash/issues">Report Bug</a>
+    .
+    <a href="https://github.com/swiftly-solution/swiftly_antiflash/issues">Request Feature</a>
+  </p>
+</p>
+
+
+### Installation 👀
+
+1. Download the newest [release](https://github.com/swiftly-solution/swiftly_antiflash/releases).
+2. Everything is drag & drop, so i think you can do it!
+
+
+
+### Have ideas? 💡
+Join [Swiftly Discord Server](https://swiftlycs2.net/discord)!
---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <swiftly/swiftly.h>
+#include <swiftly/server.h>
+#include <swiftly/database.h>
+#include <swiftly/commands.h>
+#include <swiftly/configuration.h>
+#include <swiftly/logger.h>
+#include <swiftly/timers.h>
+#include <swiftly/gameevents.h>
+
+Server *server = nullptr;
+PlayerManager *g_playerManager = nullptr;
+Database *db = nullptr;
+Commands *commands = nullptr;
+Configuration *config = nullptr;
+Logger *logger = nullptr;
+Timers *timers = nullptr;
+
+void OnProgramLoad(const char *pluginName, const char *mainFilePath)
+{
+    Swiftly_Setup(pluginName, mainFilePath);
+
+    server = new Server();
+    g_playerManager = new PlayerManager();
+    commands = new Commands(pluginName);
+    config = new Configuration();
+    logger = new Logger(mainFilePath, pluginName);
+    timers = new Timers();
+}
+
+
+
+void OnPlayerBlind(Player *player, Player *attacker, short entityid, float duration)
+{
+    if (player->team->Get() == attacker->team->Get() && player != attacker)
+    {
+        player->SetFlashDuration(0.0f);
+    }
+    else if (player == attacker)
+    {
+        return;
+    }
+}
+
+void OnPluginStart()
+{
+}
+
+void OnPluginStop()
+{
+}
+
+const char *GetPluginAuthor()
+{
+    return "";
+}
+
+const char *GetPluginVersion()
+{
+    return "1.0.0";
+}
+
+const char *GetPluginName()
+{
+    return "swiftly_antiflash";
+}
+
+const char *GetPluginWebsite()
+{
+    return "";
+}
